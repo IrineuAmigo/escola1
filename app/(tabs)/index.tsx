16400aa114c0b0e03880fbc18f298f5d0d28571a
Irineu Aparecido Amigo <ra2457078>
2026-03-18 21:27:22 -0300

    incluir tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+import { startMapper } from "react-native-reanimated";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      
+      <Text style={styles.text}>Página Inicial. </Text>
+      <Link href="/about" style={styles.button}>Página Sobre</Link> 
+      </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: '#fff',
+  },
+  button:{
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  }
+  
+}); 
+  
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,15 +1,17 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { View, StyleSheet } from "react-native";
+import { Link, Stack } from "expo-router";
 
-export default function Index() {
+export default function NotFound() {
   return (
+    <>
+    <Stack.Screen options={{ title: 'Ops! Esta Página não foi encontrada' }} />
     <View style={styles.container}>
-      
-      <Text style={styles.text}>Página Inicial. </Text>
-      <Link href="/about" style={styles.button}>Página Sobre</Link> 
+      <Link href="/" style={styles.button}>Voltar para a Página Inicial</Link>
       </View>
+    </>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
